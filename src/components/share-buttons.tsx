e1247f84a4f8a93a8c66a6cd267a7ae559b5fc7a
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-12-28 16:57:51 +0900

    feat: add share buttons on left side

---
+++ src/components/share-buttons.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  HatenaIcon,
+  HatenaShareButton,
+} from 'react-share'
+
+type Props = {
+  text: string
+  url: string
+}
+
+const ShareButtons = ({ text, url }: Props) => {
+  return (
+    <>
+      <ul>
+        <li className="mr-8">
+          <TwitterShareButton url={url} title={text}>
+            <TwitterIcon size={32} round={true}/>
+          </TwitterShareButton>
+        </li>
+        <li>
+          <FacebookShareButton url={url}>
+            <FacebookIcon size={32} round={true}/>
+          </FacebookShareButton>
+        </li>
+        <li>
+          <HatenaShareButton url={url}>
+            <HatenaIcon size={32} round={true}/>
+          </HatenaShareButton>
+        </li>
+      </ul>
+      <style jsx>{`
+      ul {
+        list-style: none;
+      }
+      `}</style>
+    </>
+  )
+}
+
+export default ShareButtons
