46be70f38681795ca9d15591850657cf0e2c96af
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-12-29 11:09:01 +0900

    feat: add web bookmark renderer

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -233,6 +233,50 @@ const RenderPost = ({ post, redirect, preview, baseBlogUrl }) => {
                 </Heading>
               )
             }
+            const renderBookmark = ({ link, title, description, format }) => {
+              const { bookmark_icon: icon, bookmark_cover: cover } = format
+              console.debug(JSON.stringify(cover))
+              toRender.push(
+                <div className={blogStyles.bookmark}>
+                  <div>
+                    <div style={{ display: 'flex' }}>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={blogStyles.bookmarkContentsWrapper}
+                        href={link}
+                      >
+                        <div role="button" className={blogStyles.bookmarkContents}>
+                          <div className={blogStyles.bookmarkInfo}>
+                            <div className={blogStyles.bookmarkTitle}>{title}</div>
+                            <div className={blogStyles.bookmarkDescription}>
+                              {description}
+                            </div>
+                            <div className={blogStyles.bookmarkLinkWrapper}>
+                              <img
+                                src={icon}
+                                className={blogStyles.bookmarkLinkIcon}
+                              />
+                              <div className={blogStyles.bookmarkLink}>{link}</div>
+                            </div>
+                          </div>
+                          <div className={blogStyles.bookmarkCoverWrapper1}>
+                            <div className={blogStyles.bookmarkCoverWrapper2}>
+                              <div className={blogStyles.bookmarkCoverWrapper3}>
+                                <img
+                                  src={cover}
+                                  className={blogStyles.bookmarkCover}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>,
+              )
+            }
 
             switch (type) {
               case 'page':
@@ -415,6 +459,12 @@ const RenderPost = ({ post, redirect, preview, baseBlogUrl }) => {
                     </components.Equation>
                   )
                 }
+                break
+              }
+              case 'bookmark': {
+                const { link, title, description } = properties
+                const { format = {} } = value
+                renderBookmark({ link, title, description, format })
                 break
               }
               default:
